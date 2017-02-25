4f36e7c6e74fa5e8a229cf79fd449003e5c3801a
Joseph <jfklaszky@gmail.com>
2017-02-24 21:23:41 -0500

    added test

--- Asst1/memgrindv2.c
+++ Asst1/memgrindv2.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
 
 #include "mymalloc.h"
 
@@ -11,60 +12,94 @@ int main(int argc, char * argv[])
 	//Used for all parts
 	int successCoutner = 0;
 	int failCounter = 0;
-	int i = 0;
+	int i = 0, j = 0;
 
 	//Part A
-	char * partA[1000];
+	// char * partA[1000];
 
-	for(i = 0; i < 1000; i++)
-	{
-		partA[i] = (char*)malloc(sizeof(char));
-		if(partA[i] != NULL)
-		{
-			successCoutner++;
-		}
-		else
-		{
-			failCounter++;
-		}
-	}
+	// for(i = 0; i < 1000; i++)
+	// {
+	// 	partA[i] = (char*)malloc(sizeof(char));
+	// 	if(partA[i] != NULL)
+	// 	{
+	// 		successCoutner++;
+	// 	}
+	// 	else
+	// 	{
+	// 		failCounter++;
+	// 	}
+	// }
 
-	for(i = 0; i < successCoutner; i++)
-	{
-		free(partA[i]);
-	}
+	// for(i = 0; i < successCoutner; i++)
+	// {
+	// 	free(partA[i]);
+	// }
 
-	printf("\n\nPart A:\n");
-	printf("Successes: %d\nFailures: %d\n", successCoutner, failCounter);
+	// printf("\n\nPart A:\n");
+	// printf("Successes: %d\nFailures: %d\n", successCoutner, failCounter);
 
-	//Part B
-	char * partB;
-	successCoutner = 0;
-	failCounter = 0;
+	// //Part B
+	// char * partB;
+	// successCoutner = 0;
+	// failCounter = 0;
 
-	for(i = 0; i < 1000; i++)
-	{
-		partB = (char*)malloc(sizeof(char*));
-		if(partB != NULL)
-		{
-			successCoutner++;
-		}
-		else
-		{
-			failCounter++;
-		}
+	// for(i = 0; i < 1000; i++)
+	// {
+	// 	partB = (char*)malloc(sizeof(char*));
+	// 	if(partB != NULL)
+	// 	{
+	// 		successCoutner++;
+	// 	}
+	// 	else
+	// 	{
+	// 		failCounter++;
+	// 	}
 
-		free(partB);
-	}
+	// 	free(partB);
+	// }
 
-	printf("\n\nPart B:\n");
-	printf("Successes: %d\nFailures: %d\n", successCoutner, failCounter);
+	// printf("\n\nPart B:\n");
+	// printf("Successes: %d\nFailures: %d\n", successCoutner, failCounter);
 
 	//Part C
 
-	// char * partC[1000];
+	time_t t;
+	srand((unsigned) time(&t));
+	char * partC[1000];
+	int randomInt;
 	i = 0;
 	successCoutner = 0;
 	failCounter = 0;
+
+	while(successCoutner <= 1000)
+	{
+		randomInt = rand();
+		if( randomInt % 2 == 0)
+		{	
+			partC[i] = (char*)malloc(sizeof(char*));
+			if(partC[i] == NULL)
+			{
+				failCounter++;
+				i--;
+			}
+			else
+			{
+				successCoutner += 1;
+				i++;
+			}
+		}
+		else
+		{
+			free(partC[i]);
+			i--;
+		}
+
+		printf("\n\n%d\n\n", successCoutner);
+	}
+
+	for(j = 0; j < i; j++)
+	{
+		free(partC[j]);
+	}
 	return 0;
 }
