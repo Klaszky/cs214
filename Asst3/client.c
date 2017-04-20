0e418e202222b9bfc01c5d74202478741be72d1e
Joseph <jfklaszky@gmail.com>
2017-04-20 16:18:09 -0400

    sort of working

--- Asst3/client.c
+++ Asst3/client.c
@@ -31,32 +31,32 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	socketFD = socket(AF_INET, SOCK_STREAM, 0);
-	if(socketFD < 0)
-	{
-		printf("couldn't make a socket\n");
-		return -1;
-	}
-
-	bzero((char *) &serverAddressInfo, sizeof(serverAddressInfo));
-
-	serverAddressInfo.sin_family = AF_INET;
-	serverAddressInfo.sin_port = htons(portNum);
-
-	bcopy((char *) serverIPAddress->h_addr, (char *)&serverAddressInfo.sin_addr.s_addr, serverIPAddress->h_length);
-
-	if(connect(socketFD, (struct sockaddr *)&serverAddressInfo, sizeof(serverAddressInfo)) < 0)
-	{
-		printf("couldn't connect \n");
-		return -1;
-	}
 	while(1)
 	{
+		socketFD = socket(AF_INET, SOCK_STREAM, 0);
+		if(socketFD < 0)
+		{
+			printf("couldn't make a socket\n");
+			return -1;
+		}
+
+		bzero((char *) &serverAddressInfo, sizeof(serverAddressInfo));
+
+		serverAddressInfo.sin_family = AF_INET;
+		serverAddressInfo.sin_port = htons(portNum);
+
+		bcopy((char *) serverIPAddress->h_addr, (char *)&serverAddressInfo.sin_addr.s_addr, serverIPAddress->h_length);
+
+		if(connect(socketFD, (struct sockaddr *)&serverAddressInfo, sizeof(serverAddressInfo)) < 0)
+		{
+			printf("couldn't connect \n");
+			return -1;
+		}
 		printf("Eneter a msg: ");
 		bzero(buffer, 256);
 		fgets(buffer, 255, stdin);
 
-		n = write(socketFD,buffer,strlen(buffer));
+		n = write(socketFD, buffer, strlen(buffer));
 
 		if(n < 0)
 		{
