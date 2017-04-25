182caacd93522a5c4e8fc50494d41ac291900d73
klaszky <jfklaszky@gmail.com>
2017-04-24 22:46:16 -0400

    sc

--- Asst3/libnetfiles.h
+++ Asst3/libnetfiles.h
@@ -1,1 +1,8 @@
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+// int open(const char *pathname, int flags);
+int nopen(char * buffer);
+int intLen(int x);
+char * pullString(int start, int end, int size, char * originalString);
