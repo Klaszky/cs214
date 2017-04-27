1715271aa3d5c2be835ea98b48037e5f72301ce6
Joseph <jfklaszky@gmail.com>
2017-04-27 14:44:13 -0400

    netclose is finished

--- Asst3/netclient.c
+++ Asst3/netclient.c
@@ -3,9 +3,8 @@
 int main()
 {
 	networkserverinit("grep.cs.rutgers.edu");
-	int testFD = netopen("./test", O_RDONLY);
-	printf("%d\n", testFD);
-	printf("%d\n", netclose(testFD));
+	printf("%d\n", netopen("./test", O_RDONLY));
+	printf("%d\n", netclose(-5));
 	printf("%d\n", netopen("./test", O_RDONLY));
 	printf("%d\n", netopen("./test2", O_RDONLY));
 	printf("%d\n", netopen("./testing", O_RDONLY));
