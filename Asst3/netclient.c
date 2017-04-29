a7e9d6b59fc0f3ac79f878e8cf4192c5e9af0caa
Joseph <jfklaszky@gmail.com>
2017-04-29 15:50:45 -0400

    more werid freeing issues

--- Asst3/netclient.c
+++ Asst3/netclient.c
@@ -5,7 +5,7 @@ int main()
 	char * readIn = (char*)malloc(sizeof(char) * 2000);
 	int amtToRead = 2000;
 	int status = 0;
-	// char * buf = "Testing testing one two three i am lost and i don't know what to write";
+	char * buf = "and now we're back with a new Testing testing one two three i am lost and i don't know what to write";
 
 
 	networkserverinit("grep.cs.rutgers.edu");
@@ -16,22 +16,18 @@ int main()
 	int fd = netopen("./oz.txt", O_RDONLY);
 	printf("%d\n", fd);
 
-	while(amtToRead > 0)
-	{
-		status = netread(fd, readIn, amtToRead);
-		amtToRead -= status;
-		printf("status: %d\n", status);
-		printf("amtToRead: %d\n", amtToRead);
-	}
-
-	// printf("%s\n", readIn);
+	status = netread(fd, readIn, amtToRead);
+	amtToRead -= status;
+	printf("status: %d\n", status);
+	printf("amtToRead: %d\n", amtToRead);
+	printf("%s\n", readIn);
 
 	printf("%d\n", netclose(fd));
-	// int fd = netopen("./test", O_WRONLY);
-	// status = netwrite(fd, buf, strlen(buf));
-	// printf("%d\n", status);
-	// printf("%d\n", netopen("./test2", O_RDONLY));
-	// printf("%d\n", netopen("./testing", O_RDONLY));
+	int fd2 = netopen("./test", O_WRONLY);
+	status = netwrite(fd2, buf, strlen(buf));
+	printf("status 2: %d\n", status);
+	printf("%d\n", netopen("./test2", O_RDONLY));
+	printf("%d\n", netopen("./testing", O_RDONLY));
 
 	return 0;
 }
